4e630d2bea27dc44827d817e0f203806f258e7ba
EugeneBuchkovsky <buchkovsyeugene20th@gmail.com>
2015-10-22 13:20:07 +0200

    Polinomial v.1

---
+++ Task1.Polinomial.Test/PolinomialTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task1.Polinomial.Test
+{
+    [TestClass]
+    public class PolinomialTest
+    {
+        [TestMethod]
+        public void Sum_123and55_return178()
+        {
+            Polinomial p1 = new Polinomial(1,2,3);
+            Polinomial p2 = new Polinomial(5,5);
+            Polinomial expectedP = new Polinomial(1,7,8);
+
+            Polinomial result = p1 + p2;
+
+            Assert.AreEqual(expectedP , result);   
+        }
+
+        [TestMethod]
+        public void Sub_55and123_returnM132()
+        {
+            Polinomial p1 = new Polinomial(1, 2, 3);
+            Polinomial p2 = new Polinomial(5, 5);
+            Polinomial expectedP = new Polinomial(-1, 3, 2);
+
+            Polinomial result = p2 - p1;
+
+            Assert.AreEqual(expectedP, result);
+        }
+
+        [TestMethod]
+        public void Sum_123and45_return178()
+        {
+            Polinomial p1 = new Polinomial(1, 2, 3);
+            Polinomial p2 = new Polinomial(4, 5);
+            Polinomial expectedP = new Polinomial(4, 13, 22, 15, 0 ,0 );
+
+            Polinomial result = p1 * p2;
+
+            Assert.AreEqual(expectedP, result);
+        }
+
+        [TestMethod]
+        public void Equals_123and321_returnFalse()
+        {
+            Polinomial p1 = new Polinomial(1, 2, 3);
+            Polinomial p2 = new Polinomial(3, 2, 1);
+            bool expectedResult = false;
+
+            bool result = Polinomial.Equals(p1, p2);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Equals_123and123_returnTrue()
+        {
+            Polinomial p1 = new Polinomial(1, 2, 3);
+            Polinomial p2 = new Polinomial(1, 2, 3);
+            bool expectedResult = true;
+
+            bool result = Polinomial.Equals(p1, p2);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void Equals_123_returnTrue()
+        {
+            Polinomial p1 = new Polinomial(1, 2, 3);
+            Polinomial p2 = p1;
+            bool expectedResult = true;
+
+            bool result = Polinomial.Equals(p1, p2);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Task1.Polinomial/Polinomial.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Polinomial
+{
+    public class Polinomial : IEquatable<Polinomial>
+    {
+        /// <summary>
+        /// Store polinomial.
+        /// </summary>
+        private double[] CoefficientArray;
+
+        public Polinomial(params double[] source)
+        {
+            CoefficientArray = source;
+        }
+
+        
+        /// <summary>
+        /// Addition of polinomials.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static Polinomial Add(Polinomial x, Polinomial y)
+        {
+            return new Polinomial(SumPolynom(x.CoefficientArray, y.CoefficientArray));
+        }
+
+        /// <summary>
+        /// Substraction of polinomials.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static Polinomial Substract(Polinomial x, Polinomial y)
+        {
+            double[] negY = new double[y.CoefficientArray.Length];
+            for (int i = 0; i < y.CoefficientArray.Length; i++)
+                negY[i] = y.CoefficientArray[i] * (-1);
+            return new Polinomial(SumPolynom(x.CoefficientArray, negY));      
+        }
+
+        /// <summary>
+        /// Myltiply of polinomials.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static Polinomial Multiply(Polinomial x, Polinomial y)
+        {
+            double[] result = new double[x.CoefficientArray.Length * y.CoefficientArray.Length];
+            for (int i=0;i<x.CoefficientArray.Length;i++)
+                for (int j = 0; j < y.CoefficientArray.Length; j++)
+                    result[i + j] += x.CoefficientArray[i] * y.CoefficientArray[j];
+            return new Polinomial(result);
+        }
+
+        /// <summary>
+        /// Overloaded addition operator.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static Polinomial operator +(Polinomial x, Polinomial y)
+        {
+            return Add(x, y);
+        }
+
+        /// <summary>
+        /// Overloaded substraction operator.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static Polinomial operator -(Polinomial x, Polinomial y)
+        {
+            return Substract(x, y);
+        }
+
+        /// <summary>
+        /// Overloaded miltiply operator.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static Polinomial operator *(Polinomial x, Polinomial y)
+        {
+            return Multiply(x, y);
+        }
+
+        /// <summary>
+        /// Method GetHashCode System.Object
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return (int)CoefficientArray.Sum();
+        }
+
+        /// <summary>
+        /// Overloaded method Equals System.Object
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Polinomial)
+                return Equals((Polinomial)obj);
+            return false;
+        }
+
+        public bool Equals(Polinomial p1)
+        {
+            return CoefficientArray.SequenceEqual(p1.CoefficientArray);
+        }
+
+        public static bool Equals(Polinomial p1, Polinomial p2)
+        {
+            return p1.CoefficientArray.SequenceEqual(p2.CoefficientArray);
+        }
+
+        /// <summary>
+        /// Overloaded equality operator.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator ==(Polinomial x, Polinomial y)
+        {
+            return Equals(x, y);
+        }
+
+        /// <summary>
+        /// Overloaded inequality operator.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator !=(Polinomial x, Polinomial y)
+        {
+            return !Equals(x, y);
+        }
+
+        /// <summary>
+        /// String representation Polinomial.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            StringBuilder temp = new StringBuilder();
+            for (int i = 0; i < CoefficientArray.Length; i++)
+            {
+                if (CoefficientArray[i]!=0)
+                    temp.Append(String.Format("{0}x^{1}", CoefficientArray[i], (CoefficientArray.Length - 1 - i)));
+                if (CoefficientArray.Length - 1 - i > 0)
+                    temp.Append(" + ");
+            }
+            return temp.ToString();
+        }
+
+        /// <summary>
+        /// Method summation arrays-polinomias.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private static double[] SumPolynom(double[] x, double[] y)
+        {
+            double[] result;
+
+            if (x.Length > y.Length)
+            {
+                result = (double[])x.Clone();
+                for (int i = 0; i < y.Length; i++)
+                    result[result.Length - 1 - i] += y[y.Length - 1 - i];
+            }
+            else
+            {
+                result = (double[])y.Clone();
+                for (int i = 0; i < x.Length; i++)
+                    result[result.Length - 1 - i] += x[x.Length - 1 - i];
+            }
+            return result;
+        }
+    }
+}
